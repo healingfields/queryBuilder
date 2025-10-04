b428e5e93226cc057b4791f56f969368a1f70a7e
idriss <dihratene@gmail.com>
2025-10-04 23:04:08 +0100

    created api js

---
+++ frontend/queryBuilder/src/api.js
@@ -0,0 +1,20 @@
+const BASE_URL = "https://localhost:8080";
+
+export async function getTables(){
+    const res = await fetch(`${BASE_URL}/tables`);
+    return res.json();
+}
+
+export async function getTableColumns(table){
+    const res = await fetch(`${BASE_URL}/columns?table=${table}`);
+    return res.json();
+}
+
+export async function executeQuery(query){
+    const res = await fetch(`${BASE_URL}/query`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(query)
+    });
+    return res.json();
+}
