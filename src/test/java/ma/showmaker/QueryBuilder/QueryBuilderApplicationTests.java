4e3f64e4af21e06c7ad57619a7b31f6dbde4e985
idriss <dihratene@gmail.com>
2025-10-04 16:55:50 +0100

    initialized the project and set up the database config

---
+++ src/test/java/ma/showmaker/QueryBuilder/QueryBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.showmaker.QueryBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
