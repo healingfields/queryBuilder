24bfdee02d29605d0265e4c01eebddfa86622cdb
idriss <dihratene@gmail.com>
2025-10-18 23:14:25 +0100

    adjusted the endpoint to handle different type of columns

--- backend/src/main/java/ma/showmaker/QueryBuilder/controller/QueryController.java
+++ backend/src/main/java/ma/showmaker/QueryBuilder/controller/QueryController.java
@@ -1,61 +1,51 @@
 package ma.showmaker.QueryBuilder.controller;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
+import jakarta.persistence.EntityNotFoundException;
 import ma.showmaker.QueryBuilder.dto.FilterDto;
 import ma.showmaker.QueryBuilder.dto.QueryRequestDto;
 import ma.showmaker.QueryBuilder.dto.QueryResponseDto;
+import ma.showmaker.QueryBuilder.model.DatabaseSchema;
+import ma.showmaker.QueryBuilder.model.TableSchema;
+import ma.showmaker.QueryBuilder.service.QueryService;
 import ma.showmaker.QueryBuilder.service.SchemaService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 @RestController
 public class QueryController {
 
     private final SchemaService schemaService;
+    private final QueryService queryService;
 
-    public QueryController(SchemaService schemaService) {
+    public QueryController(SchemaService schemaService, QueryService queryService) {
         this.schemaService = schemaService;
+        this.queryService = queryService;
     }
 
     @PostMapping("/query")
     public QueryResponseDto<Object> executeQuery(@RequestBody QueryRequestDto queryRequestDto){
+        DatabaseSchema databaseSchema = schemaService.getDatabaseSchema();
+        TableSchema tableSchema = databaseSchema
+                .getTables()
+                .stream()
+                .filter(ts -> Objects.equals(ts.getName(), queryRequestDto.getTable()))
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("table not found"));
         String table = queryRequestDto.getTable();
-
         String selectedColumns = (queryRequestDto.getColumns() == null || queryRequestDto.getColumns().isEmpty())
                 ? "*"
                 : String.join(", ", queryRequestDto.getColumns());
-        // [age, name] age, name
 
         StringBuilder statement = new StringBuilder("select " + selectedColumns + " from " + table);
 
-        //where name <>=like 'reda'
-        //adding filters
+        StringBuilder filters = new StringBuilder();
         if(queryRequestDto.getFilters() != null || !queryRequestDto.getFilters().isEmpty()){
-            statement.append(" where");
-            for(int i = 0; i < queryRequestDto.getFilters().size(); i++){
-                FilterDto filter = queryRequestDto.getFilters().get(i);
-
-                statement.append(" ")
-                        .append(filter.getColumn())
-                        .append(" ")
-                        .append(filter.getOperator())
-                        .append(" ");
-                //this needs to be handled in a better way
-                if (filter.getColumn().equalsIgnoreCase("amount") ||
-                        filter.getColumn().equalsIgnoreCase("age") || filter.getColumn().equalsIgnoreCase("id")) {
-                    statement.append(filter.getValue()); // numeric
-                } else {
-                    statement.append("'").append(filter.getValue()).append("'"); // string
-                }
-                if(i < queryRequestDto.getFilters().size() - 1){
-                    statement.append(" AND");
-                }
-            }
+            filters = queryService.createFilters(tableSchema.getColumns(), queryRequestDto.getFilters());
         }
+        statement.append(filters);
         return QueryResponseDto.builder()
                 .query(statement.toString())
                 .build();
---
+++ backend/src/main/java/ma/showmaker/QueryBuilder/service/QueryService.java
@@ -0,0 +1,35 @@
+package ma.showmaker.QueryBuilder.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import ma.showmaker.QueryBuilder.dto.FilterDto;
+import ma.showmaker.QueryBuilder.model.ColumnSchema;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.logging.Filter;
+import java.util.stream.Collectors;
+
+@Service
+public class QueryService {
+
+    public StringBuilder createFilters(List<ColumnSchema> columns, List<FilterDto> filters){
+        String conditions = filters
+                .stream()
+                .map(filterDto -> String.format("%s %s %s",
+                        filterDto.getColumn(),
+                        filterDto.getOperator(),
+                        parseFilterValue(filterDto, columns)))
+                .collect(Collectors.joining(" AND "));
+        return new StringBuilder(" WHERE ").append(conditions);
+    }
+
+    private String parseFilterValue(FilterDto filter, List<ColumnSchema> columns) {
+        ColumnSchema columnSchema1 = columns
+                .stream()
+                .filter(columnSchema -> Objects.equals(filter.getColumn(), columnSchema.getName()))
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("column not found"));
+        return columnSchema1.getType().equalsIgnoreCase("String") ? String.format("' %s '", filter.getValue()) : filter.getValue();
+    }
+}
